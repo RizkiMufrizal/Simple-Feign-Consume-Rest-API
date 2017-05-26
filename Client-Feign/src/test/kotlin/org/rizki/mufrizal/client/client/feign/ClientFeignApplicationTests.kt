398b80bc5c948f968c4e7fe647bd2f7364afe418
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-05-27 00:27:09 +0700

    add test

---
+++ Client-Feign/src/test/kotlin/org/rizki/mufrizal/client/client/feign/ClientFeignApplicationTests.kt
@@ -0,0 +1,16 @@
+package org.rizki.mufrizal.client.client.feign
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ClientFeignApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
