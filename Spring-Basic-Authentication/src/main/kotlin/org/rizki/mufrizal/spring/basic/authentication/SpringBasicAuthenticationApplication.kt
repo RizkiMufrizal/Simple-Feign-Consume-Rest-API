6dae139e4af64bf4955cccff05783c7b157ad457
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-05-27 00:23:43 +0700

    add main class

---
+++ Spring-Basic-Authentication/src/main/kotlin/org/rizki/mufrizal/spring/basic/authentication/SpringBasicAuthenticationApplication.kt
@@ -0,0 +1,11 @@
+package org.rizki.mufrizal.spring.basic.authentication
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringBasicAuthenticationApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(SpringBasicAuthenticationApplication::class.java, *args)
+}
