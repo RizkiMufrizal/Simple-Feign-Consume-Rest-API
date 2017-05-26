c3667b357c5320863ebbf32abb4b62f21a05321e
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-05-27 00:28:18 +0700

    add domain response user

---
+++ Client-Feign/src/main/kotlin/org/rizki/mufrizal/client/client/feign/domain/User.kt
@@ -0,0 +1,6 @@
+package org.rizki.mufrizal.client.client.feign.domain
+
+/**
+ * Created by rizkimufrizal on 5/25/17.
+ */
+data class User (var username: String? = null, var password: String? = null)
