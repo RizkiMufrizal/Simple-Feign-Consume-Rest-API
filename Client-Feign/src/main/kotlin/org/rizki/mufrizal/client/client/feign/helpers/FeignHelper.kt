ec25359fc8c6f9dc2f22d53292af0d5cda82695a
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-05-27 00:28:36 +0700

    add feign helper class

---
+++ Client-Feign/src/main/kotlin/org/rizki/mufrizal/client/client/feign/helpers/FeignHelper.kt
@@ -0,0 +1,17 @@
+package org.rizki.mufrizal.client.client.feign.helpers
+
+import feign.Feign
+import feign.auth.BasicAuthRequestInterceptor
+import feign.jackson.JacksonDecoder
+import feign.jackson.JacksonEncoder
+
+/**
+ * Created by rizkimufrizal on 5/25/17.
+ */
+
+class FeignHelper {
+    companion object {
+        fun <T> feignBuilder(clz: Class<T>): T = Feign.builder().encoder(JacksonEncoder()).decoder(JacksonDecoder()).target(clz, "http://localhost:8081")
+        fun <T> feignBuilderBasicAuthentication(clz: Class<T>, username: String, password: String): T = Feign.builder().encoder(JacksonEncoder()).decoder(JacksonDecoder()).requestInterceptor(BasicAuthRequestInterceptor(username, password)).target(clz, "http://localhost:8081")
+    }
+}
