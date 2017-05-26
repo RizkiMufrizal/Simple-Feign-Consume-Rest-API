ea9c554fe8ad5177391bb9d781fad3f932c2fdda
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-05-27 00:22:57 +0700

    add sample test

---
+++ Spring-Basic-Authentication/src/test/kotlin/org/rizki/mufrizal/spring/basic/authentication/SpringBasicAuthenticationApplicationTests.kt
@@ -0,0 +1,16 @@
+package org.rizki.mufrizal.spring.basic.authentication
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SpringBasicAuthenticationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
