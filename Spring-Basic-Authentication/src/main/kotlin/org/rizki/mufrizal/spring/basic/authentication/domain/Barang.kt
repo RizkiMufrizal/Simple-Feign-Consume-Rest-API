94278e590454075fab0af6a15ba185c18bb86c26
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-05-27 13:32:37 +0700

    update domain barang

--- Spring-Basic-Authentication/src/main/kotlin/org/rizki/mufrizal/spring/basic/authentication/domain/Barang.kt
+++ Spring-Basic-Authentication/src/main/kotlin/org/rizki/mufrizal/spring/basic/authentication/domain/Barang.kt
@@ -1,7 +1,6 @@
 package org.rizki.mufrizal.spring.basic.authentication.domain
 
-import java.util.Date
-import kotlin.reflect.KClass
+import java.math.BigDecimal
 
 /**
  * Created by rizkimufrizal on 5/25/17.
@@ -11,4 +10,4 @@ enum class JenisBarang {
     cair, pada, gas
 }
 
-data class Barang(val idBarang: String? = null, val namaBarang: String? = null, val jumlahBarang: Int? = null, val tanggalKadaluarsa: Date? = null, val jenisBarang: JenisBarang? = null)
+data class Barang(val idBarang: String? = null, val namaBarang: String? = null, val jumlahBarang: Int? = null, val jenisBarang: JenisBarang? = null, val hargaBarang: BigDecimal? = null)
