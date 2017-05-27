0f194f8a6881284608b188b87a5bb0782c91c435
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-05-27 13:33:54 +0700

    add username

--- Client-Feign/src/main/kotlin/org/rizki/mufrizal/client/client/feign/controller/IndexController.kt
+++ Client-Feign/src/main/kotlin/org/rizki/mufrizal/client/client/feign/controller/IndexController.kt
@@ -1,7 +1,9 @@
 package org.rizki.mufrizal.client.client.feign.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import javax.servlet.http.HttpSession
 
 /**
  * Created by rizkimufrizal on 5/25/17.
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping
 class IndexController {
 
     @GetMapping(value = "/")
-    fun index(): String = "index"
+    fun index(httpSession: HttpSession, model: Model): String {
+        model.addAttribute("username", httpSession.getAttribute("username"))
+        return "index"
+    }
 
 }
