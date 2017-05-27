b173f8181ffd49e28a0b1271564418f1a789c25a
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-05-27 14:11:54 +0700

    add example data with commandlineRunner

--- Spring-Basic-Authentication/src/main/kotlin/org/rizki/mufrizal/spring/basic/authentication/SpringBasicAuthenticationApplication.kt
+++ Spring-Basic-Authentication/src/main/kotlin/org/rizki/mufrizal/spring/basic/authentication/SpringBasicAuthenticationApplication.kt
@@ -1,10 +1,31 @@
 package org.rizki.mufrizal.spring.basic.authentication
 
+import org.rizki.mufrizal.spring.basic.authentication.controller.BarangController
+import org.rizki.mufrizal.spring.basic.authentication.domain.Barang
+import org.rizki.mufrizal.spring.basic.authentication.domain.JenisBarang
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import java.math.BigDecimal
 
 @SpringBootApplication
-class SpringBasicAuthenticationApplication
+class SpringBasicAuthenticationApplication {
+    @Bean
+    fun runner(): CommandLineRunner {
+        return CommandLineRunner {
+            for (i in 0..10) {
+                BarangController.barangs.add(Barang(
+                        idBarang = "B00$i",
+                        hargaBarang = BigDecimal(i + 1000),
+                        jenisBarang = if (i % 2 == 0) JenisBarang.cair else JenisBarang.padat,
+                        jumlahBarang = i + 10,
+                        namaBarang = "Barang-00$i"
+                ))
+            }
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(SpringBasicAuthenticationApplication::class.java, *args)
